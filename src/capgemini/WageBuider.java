64998e5cb548480eef0f3756b9b84897ed2cb3f1
mehakjit <mehakjitsingh@gmail.com>
2020-09-26 05:26:36 +0530

    [Mehakjit] Created: Daily Wage Calculation

--- src/capgemini/WageBuider.java
+++ src/capgemini/WageBuider.java
@@ -1,15 +1,30 @@
 package capgemini;
 
 public class WageBuider {
+	public static final int Present = 0;
+	public static int a = 0;
+	
 	public static String attendence(){
 		int check_attendence = (int)(Math.random()*2);
-		if(check_attendence==0)
-			return "Employee is present ";
-			else
-			return "Employee is absent ";		
+		if(check_attendence == Present)
+			return "Employee is present";
+			else {
+				a = 1;
+				return "Employee is absent"; 	
+			}
 	}
+	
+	public static int empWage(int empStatus) {
+		int hours = 8;
+		int wage = 20;
+		if(empStatus == Present)
+			return hours*wage;
+		else
+			return 0;				
+	}
+	
 	public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation Program ");
-	System.out.println(attendence());
+	System.out.println(attendence() + " and the total wage of employee is: " + empWage(a));
 	}
 }
