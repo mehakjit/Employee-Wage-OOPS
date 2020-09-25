13652232d5aea3740b04cfafcf16e4eaf460544d
mehakjit <mehakjitsingh@gmail.com>
2020-09-26 04:04:04 +0530

    [Mehakjit] Created: Present or Absent scenerio

--- src/capgemini/WageBuider.java
+++ src/capgemini/WageBuider.java
@@ -1,8 +1,16 @@
 package capgemini;
 
 public class WageBuider {
-
+	public static String attendence(){
+		int check_attendence = (int)(Math.random()*2);
+		if(check_attendence==0)
+			return "Employee is present ";
+			else
+			return "Employee is absent ";		
+	}
 	public static void main(String[] args) {
-	System.out.println(" Welcome to Employee Wage Computation Program ");
+	System.out.println("Welcome to Employee Wage Computation Program ");
+	String status = WageBuider.attendence();
+	System.out.println(status);
 	}
 }
