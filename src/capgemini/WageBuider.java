77d030acbc0eb0e7b965dee4c6d2a7b5e787e353
mehakjit <mehakjitsingh@gmail.com>
2020-09-26 05:58:55 +0530

    [Mehakjit] Added Part Time Scenerio

--- src/capgemini/WageBuider.java
+++ src/capgemini/WageBuider.java
@@ -2,29 +2,49 @@ package capgemini;
 
 public class WageBuider {
 	public static final int Present = 0;
+	public static final int Full_Time = 0;
 	public static int a = 0;
+	public static int b = 0;
 	
 	public static String attendence(){
 		int check_attendence = (int)(Math.random()*2);
 		if(check_attendence == Present)
-			return "Employee is present";
+			return "Present";
 			else {
 				a = 1;
-				return "Employee is absent"; 	
+				return "Absent"; 	
 			}
 	}
 	
-	public static int empWage(int empStatus) {
-		int hours = 8;
-		int wage = 20;
-		if(empStatus == Present)
-			return hours*wage;
+	public static String employeeType(int empStatus) {
+		if (empStatus == Present){
+		int TypeOfEmployee = (int)(Math.random()*2);
+		if (TypeOfEmployee==Full_Time)
+			return "Full Time ";
+		else {
+			b=1;
+			return "Half Time ";
+		}}
 		else
-			return 0;				
+			return "-NA- ";
 	}
 	
+	public static int empWage(int empStatus, int empType) {
+		int hours = 8;
+		int wage = 20;
+		if(empStatus == Present) {
+			if (empType == Full_Time)
+				return hours*wage;
+			else
+				return hours*wage/2;
+		}
+		else
+			return 0;
+	}
+	
+
 	public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation Program ");
-	System.out.println(attendence() + " and the total wage of employee is: " + empWage(a));
+	System.out.println("Employee Attendence: " + attendence() + " Employe Type: " + employeeType(a) + " Employee Wage: " + empWage(a,b));
 	}
 }
