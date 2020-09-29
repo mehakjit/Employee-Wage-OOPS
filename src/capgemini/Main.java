1f0d2584ea535bf8b02bed4b6dc66160a9216981
mehakjit <mehakjitsingh@gmail.com>
2020-09-29 05:37:30 +0530

    [Mehakjit] Created: Save location for multiple companies

--- src/capgemini/Main.java
+++ src/capgemini/Main.java
@@ -2,9 +2,11 @@ package capgemini;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program ");
+		
 		WageBuider j = new WageBuider();
-	System.out.println("Welcome to Employee Wage Computation Program ");
-	System.out.println(j.Wage("Capgemini",50,100,20));
-	System.out.println(j.Wage("Capgemini",50,100,50));
+		System.out.println(j.Wage("Capgemini",50,100,20));
+		System.out.println(j.Wage("Elante",50,200,50));
+
 }
 }
--- src/capgemini/WageBuider.java
+++ src/capgemini/WageBuider.java
@@ -2,42 +2,39 @@ package capgemini;
 
 public class WageBuider {
 	public static final int PRESENT = 0;
-	public static final int Abscent = 1;
 	public static final int Full_Time = 0;
 	public static final int Half_Time = 1;
-	public static int a = 0;
-	public static int b = 0;
+	public static int tempStaus = 0;
+	public static int tempType = 0;
 
-	public static String attendence(){
+	public String attendence(){
 		int check_attendence = (int)(Math.random()*2);
 		switch(check_attendence) {
 		case PRESENT:
 			return "Present";
-		case Abscent:
-			a=1;
+		default :
+			tempStaus=1;
 			return "Abscent";	
 		}
-	return "dchusuygfuyws";
 	}
 
-	public static String employeeType(int empStatus) {
+	public String employeeType(int empStatus) {
 		switch (empStatus) {
 		case PRESENT:
 			int TypeOfEmployee = (int)(Math.random()*2);
 				switch (TypeOfEmployee) {
 				case Full_Time:
-					return"Full Time "; 
+					return "Full Time "; 
 				case Half_Time:
-					b=1;
+					tempType=1;
 					return "Half Time "; 
 				}
-			break;
-		case Abscent:
-			return("-NA- ");
+		default :
+			return "-NA- ";
 	}
-		return " ush";}
+		}
 
-	public static int empWage(int empStatus, int empType ,int empWage) {
+	public int empWage(int empStatus, int empType ,int empWage) {
 		int hours = 8;
 		switch(empStatus) {
 		case PRESENT:
@@ -47,9 +44,8 @@ public class WageBuider {
 			case Half_Time:
 				return hours*empWage/2;
 		}
-		case Abscent:
+		default :
 			return 0;}
-		return 0;
 	}
 	public String Wage(String company,int no_Of_Days,int max_no_of_hours,int empWage) {
 		int totalWage = 0;
@@ -59,15 +55,13 @@ public class WageBuider {
 			while (hours<max_no_of_hours && day<no_Of_Days) {
 			day += 1;
 			System.out.println("Company: " + company + ";   Day: " + day +";   Attendence: "+ attendence() + ";   Employe Type: " 
-								+ employeeType(a) + ";   Employee Wage: " + empWage(a,b,empWage) );
-			hours +=  (empWage(a,b,empWage)/empWage);
-			totalWage += empWage(a, b,empWage);
-			a=0;
-			b=0;
+								+ employeeType(tempStaus) + ";   Employee Wage: " + empWage(tempStaus,tempType,empWage) );
+			hours +=  (empWage(tempStaus,tempType,empWage)/empWage);
+			totalWage += empWage(tempStaus, tempType,empWage);
+			tempStaus=0;
+			tempType=0;
 			}
 		}
 		return "Total Wage: " + totalWage;
 	}
-
-
-	}
+}
